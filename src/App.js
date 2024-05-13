35d7afd4fa8048686a0279f3bd5e29d02e2647c4
arifwp <arifwahyu696@gmail.com>
2024-05-13 14:18:18 +0700

    simple routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import {
+  createBrowserRouter,
+  // createRoutesFromElements,
+  RouterProvider,
+  // Route,
+} from "react-router-dom";
+import HomePage from "./components/Home";
+import ProductsPage from "./components/Product";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  {
+    path: "/products",
+    element: <ProductsPage />,
+  },
+]);
+
+// const router = createBrowserRouter(routeDefinitions);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
